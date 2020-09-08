853c2531e6432eab57f623ad2501df553a71ae74
daliborstakic <stakicdalibor64@gmail.com>
2020-09-08 15:17:54 +0200

    added elements to the grid

--- gui_rand.py
+++ gui_rand.py
@@ -1,5 +1,19 @@
 """ Importing Tkinter """
 import tkinter as tk
+from tkinter import Button, Label
 
 # Main root window
 root = tk.Tk()
+
+# Label 
+button_label = Label(root, text="Press the button to generate a Wikipedia page")
+
+# Button
+gen_button = Button(root, text="Generate")
+
+# Grid
+button_label.grid(row=0, column=0, padx=5, pady=5)
+gen_button.grid(row=1, column=0, pady=5, padx=5)
+
+if __name__ == '__main__':
+    root.mainloop()
