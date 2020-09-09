21711a2d34e3eec1daead3138fcfbfef15a73e29
daliborstakic <stakicdalibor64@gmail.com>
2020-09-09 10:40:22 +0200

    added gen_rand method

---
+++ wiki_app.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+""" Simulating keyboard shortcuts """
+from selenium.webdriver.common.keys import Keys
+
+# Webdriver
+PATH = "C:\Program Files (x86)\chromedriver.exe" # Path
+driver = webdriver.Chrome(PATH)
+
+# Loading the page
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+html_page = driver.find_element_by_xpath('html')
+
+def gen_rand():
+    """ Generates a random Wiki page """
+    html_page.send_keys(Keys.ALT, Keys.SHIFT, 'x')
